8c03414b3b483fa88034702c016ecef4afb72f43
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-05-06 20:05:21 +0800

    fix test - util.js

--- test/lib/util.js
+++ test/lib/util.js
@@ -1,7 +1,10 @@
 const chai = require("chai");
 const expect = chai.expect;
+const dirtyChai = require('dirty-chai')
+
 const util = require("../../lib/util");
 
+chai.use(dirtyChai);
 describe("the util modole", () => {
   context("the notEmpty function", () => {
     it("should return true when given a string ", () => {
